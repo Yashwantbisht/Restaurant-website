3ed78a03f1d76e9b9b3cdcfac75c6e8b685e4f43
Yashwantbisht <72188791+Yashwantbisht@users.noreply.github.com>
2020-10-02 07:26:34 +0530

    Added New Program

---
+++ Yashwant.py
@@ -0,0 +1,9 @@
+# https://facebook.com/yashwant/posts/5483901245396
+# subscribed by codehouse
+""" Calculate quare root of numbers 0 to 100"""
+from math import *
+i = 0
+while i<= 100:
+ print i, "\t\t" , sqrt(i)
+ i = i + 1
+print "READY!"
